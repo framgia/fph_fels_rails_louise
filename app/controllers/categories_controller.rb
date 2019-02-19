5a92edd93c85c0f2cf2fcc5b7790dc4f56b34eb4
louiselumapas <louise.lumapas.ll@gmail.com>
2019-02-19 18:46:52 +0800

    user category page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,6 @@
 class CategoriesController < ApplicationController
-    
+
+    def index
+        @categories = Category.paginate(page: params[:page], per_page:8).where("EXISTS(SELECT 1 from words where categories.id = words.category_id)")
+    end
 end
