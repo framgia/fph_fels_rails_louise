f7f0266ce22292823e8bd2b1cb5dbcd27bffe3e2
louiselumapas <louise.lumapas.ll@gmail.com>
2019-02-20 12:15:17 +0800

    updated user category pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
 
     def index
-        @categories = Category.paginate(page: params[:page], per_page:8).where("EXISTS(SELECT 1 from words where categories.id = words.category_id)")
+        @categories = Category.paginate(page: params[:page], per_page:8).has_words
     end
 end
