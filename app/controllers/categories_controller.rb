aeb8038b78241eab082812d440822d97a6bb11c6
wis <louise.lumapas.ll@gmail.com>
2019-02-20 14:29:08 +0800

    User category page (#6)
    * user category page
    
    * updated user category pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,6 @@
 class CategoriesController < ApplicationController
-    
+
+    def index
+        @categories = Category.paginate(page: params[:page], per_page:8).has_words
+    end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,9 @@ class Category < ApplicationRecord
   validates :description, presence: true
   default_scope -> { order(created_at: :desc) }
   validates :title, presence: true
+  
+  def self.has_words
+    Category.where(:id => Word.select(:category_id).map(&:category_id))
+  end
 end
+
