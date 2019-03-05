b20e0813c254d4d2c7077c6135d1f7dd960d9021
louiselumapas <louise.lumapas.ll@gmail.com>
2019-03-05 14:03:10 +0800

    fixed merging changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,6 @@ class User < ApplicationRecord
   has_many :answers, through: :lessons
   has_many :categories, through: :lessons
   has_many :activities, dependent: :destroy
-<<<<<<< Updated upstream
-=======
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
@@ -22,7 +20,6 @@ class User < ApplicationRecord
                                    dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
->>>>>>> Stashed changes
   validates :password, presence: true, length: { minimum: 6 },allow_nil: true
   mount_uploader :picture, PictureUploader
 
