b36b31537f48573b0099152a7ff5d09b3ad04edd
louiselumapas <louise.lumapas.ll@gmail.com>
2019-03-07 15:43:26 +0800

    user profile activity feed

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,5 @@
 class WordsController < ApplicationController
+  before_action :logged_in_user, only: [:index]  
   def index
      @answers = current_user.group_answer(params).paginate(page: params[:page], per_page:10)
   end
