fc69430c7075c194fa23a2eeaa9664b8234344c5
louiselumapas <louise.lumapas.ll@gmail.com>
2019-03-07 15:43:26 +0800

    user profile activity feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
-  class UsersController < ApplicationController   
+  class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:create, :index, :edit, :update, :dashboard, :show, :new]   
     def index
       @users = User.where.not(id: current_user.id).paginate(page: params[:page], per_page:8)
     end
